8ee6c59a5372331013387a6c1edbd1e28aa40b80
saloutos <saloutos96@gmail.com>
2024-04-09 14:51:50 -0400

    re-creating single finger code after refactor

--- Core/Src/dxl_startup.cpp
+++ Core/Src/dxl_startup.cpp
@@ -3,14 +3,11 @@
 
 extern XM430_bus dxl_bus_1;
 extern XM430_bus dxl_bus_2;
-extern XM430_bus dxl_bus_3;
 
 extern uint8_t dxl_ID1[];
 extern uint8_t dxl_ID2[];
-extern uint8_t dxl_ID3[];
 extern uint8_t idLength1;
 extern uint8_t idLength2;
-extern uint8_t idLength3;
 
 extern uint8_t DXL_MODE;
 
@@ -22,10 +19,6 @@ void Dynamixel_Shutdown_Routine(){
 	}
 	for (int i=0; i<idLength2; i++) {
 		dxl_bus_2.SetTorqueEn(dxl_ID2[i],0x00);
-		HAL_Delay(10);
-	}
-	for (int i=0; i<idLength3; i++) {
-		dxl_bus_3.SetTorqueEn(dxl_ID3[i],0x00);
 		HAL_Delay(10);
 	}
 }
@@ -84,32 +77,6 @@ void Dynamixel_Startup_Routine (bool torque_disable){
 		dxl_bus_2.SetTorqueEn(dxl_ID2[i],0x01); // to be able to move
 		HAL_Delay(100);
 	}
-	for (int i=0; i<idLength3; i++) {
-		dxl_bus_3.TurnOnLED(dxl_ID3[i], 0x00); // turn off LED
-		dxl_bus_3.SetTorqueEn(dxl_ID3[i],0x00);
-		dxl_bus_3.SetRetDelTime(dxl_ID3[i],0x02); // 4us delay time
-		dxl_bus_3.SetControlMode(dxl_ID3[i], DXL_MODE);
-		// set up indirect addresses for faster writing
-		dxl_bus_3.SetIndirectAddress(dxl_ID3[i], 168,  84); // KP
-		dxl_bus_3.SetIndirectAddress(dxl_ID3[i], 170,  85);
-		dxl_bus_3.SetIndirectAddress(dxl_ID3[i], 172,  80); // KD
-		dxl_bus_3.SetIndirectAddress(dxl_ID3[i], 174,  81);
-		dxl_bus_3.SetIndirectAddress(dxl_ID3[i], 176, 116); // goal position
-		dxl_bus_3.SetIndirectAddress(dxl_ID3[i], 178, 117);
-		dxl_bus_3.SetIndirectAddress(dxl_ID3[i], 180, 118);
-		dxl_bus_3.SetIndirectAddress(dxl_ID3[i], 182, 119);
-		dxl_bus_3.SetIndirectAddress(dxl_ID3[i], 184, 104); // goal velocity
-		dxl_bus_3.SetIndirectAddress(dxl_ID3[i], 186, 105);
-		dxl_bus_3.SetIndirectAddress(dxl_ID3[i], 188, 106);
-		dxl_bus_3.SetIndirectAddress(dxl_ID3[i], 190, 107);
-		dxl_bus_3.SetIndirectAddress(dxl_ID3[i], 192,  94); // feedforward current
-		dxl_bus_3.SetIndirectAddress(dxl_ID3[i], 194,  95);
-		// re-enable motor
-		HAL_Delay(100);
-		dxl_bus_3.TurnOnLED(dxl_ID3[i], 0x01);
-		dxl_bus_3.SetTorqueEn(dxl_ID3[i],0x01); // to be able to move
-		HAL_Delay(100);
-	}
 
 	// set smooth DXL profile
 	for (int i=0; i<idLength1; i++) {
@@ -122,29 +89,20 @@ void Dynamixel_Startup_Routine (bool torque_disable){
 		dxl_bus_2.SetAccelerationProfile(dxl_ID2[i], 100);
 		HAL_Delay(100);
 	}
-	for (int i=0; i<idLength3; i++) {
-		dxl_bus_3.SetVelocityProfile(dxl_ID3[i], 414);
-		dxl_bus_3.SetAccelerationProfile(dxl_ID3[i], 100);
-		HAL_Delay(100);
-	}
 
 	// controlled setup to send fingers to zero joint angles
 	float home_joint_pos[8] = {0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f};
 	int32_t home_motor_pos[8];
 	JointPos2MotorPos(home_joint_pos, home_motor_pos);
-	int32_t pos1[3];
-	int32_t pos2[3];
-	for (int i=0; i<3; i++) {
+	int32_t pos1[2];
+	int32_t pos2[2];
+	for (int i=0; i<2; i++) {
 		pos1[i] = home_motor_pos[i];
-		pos2[i] = home_motor_pos[i+4];
+		pos2[i] = home_motor_pos[i+2];
 	}
-	int32_t pos3[2];
-	pos3[0] = home_motor_pos[3];
-	pos3[1] = home_motor_pos[7];
 	if (!torque_disable){
 		dxl_bus_1.SetMultGoalPositions(dxl_ID1, idLength1, (uint32_t*)pos1);
 		dxl_bus_2.SetMultGoalPositions(dxl_ID2, idLength2, (uint32_t*)pos2);
-		dxl_bus_3.SetMultGoalPositions(dxl_ID3, idLength3, (uint32_t*)pos3);
 		HAL_Delay(100);
 	}
 
@@ -163,14 +121,6 @@ void Dynamixel_Startup_Routine (bool torque_disable){
 		dxl_bus_2.SetPosPGain(dxl_ID2[i], 0);
 		dxl_bus_2.SetPosDGain(dxl_ID2[i], 0);
 		if(DXL_MODE!=0x00){ dxl_bus_2.SetGoalCurrent(dxl_ID2[i], 1193); }
-		HAL_Delay(100);
-	}
-	for (int i=0; i<idLength3; i++) {
-		dxl_bus_3.SetVelocityProfile(dxl_ID3[i], 0);
-		dxl_bus_3.SetAccelerationProfile(dxl_ID3[i], 0);
-		dxl_bus_3.SetPosPGain(dxl_ID3[i], 0);
-		dxl_bus_3.SetPosDGain(dxl_ID3[i], 0);
-		if(DXL_MODE!=0x00){ dxl_bus_3.SetGoalCurrent(dxl_ID3[i], 1193); }
 		HAL_Delay(100);
 	}
 
